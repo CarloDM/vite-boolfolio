9cfabff7c3894ad7df8c9aa1f7613b9fd3683912
carloDM <carlo.de.mauro.work@gmail.com>
2023-06-26 15:19:51 +0200

    api work

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <script>
+import axios from 'axios';
 export default {
   name:'App',
   data(){
     return {
-
+      apiUrl: 'http://127.0.0.1:8000/api/',
+      posts : null,
     }
   },
-  methods:{},
+  methods:{
+    getPosts(){
+      console.log(this.apiUrl)
+      axios.get(this.apiUrl + 'post')
+        .then(result =>{
+          console.log(result.data)
+        })
+    }
+  },
   computed:{},
   mounted(){
-
+    this.getPosts()
   },
 
 }
