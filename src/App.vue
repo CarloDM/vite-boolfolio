deb6f547de86cdfdc6d3be893eb6fc33b9221eb2
carloDM <carlo.de.mauro.work@gmail.com>
2023-06-27 15:48:34 +0200

    clean

--- src/App.vue
+++ src/App.vue
@@ -3,54 +3,21 @@ import Header from './components/partials/header.vue';
 export default {
   name:'App',
   data(){
-    return {
-      // apiUrl: 'http://127.0.0.1:8000/api/',
-      // posts : null,
-    }
+    return {}
   },
   components:{Header,},
-
-  // methods:{
-  //   getPosts(){
-  //     console.log(this.apiUrl)
-  //     axios.get(this.apiUrl + 'post')
-  //       .then(result =>{
-  //         console.log(result.data);
-  //         this.posts = result.data;
-  //       })
-  //   }
-  // },
-
-  // computed:{},
-
-  // mounted(){
-  //   this.getPosts()
-  // },
-
 }
 </script>
 <template>
   <div class="main-wrapper">
+
     <Header></Header>
     <router-view></router-view>
-
-    <!-- <h2 class="m_1">posts card</h2>
-    <div class="card_container">
-
-      <ProjectCard
-      v-for=" post in posts"
-      :key="post.id"
-      :title="post.title"
-      :date="post.date"
-      :text="post.text"
-      />
-
-    </div> -->
 
   </div>
 </template>
 <style lang="scss">
-@import './scss/main.scss';
 
+@import './scss/main.scss';
 
 </style>
